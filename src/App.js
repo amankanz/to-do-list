26189411df85366412c5760a017c785380213236
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-08-02 16:50:59 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initial_todos = [
+  { text: "Buy groceries", completed: true, id: 2345 },
+  { text: "Walk the dog", completed: false, id: 2327 },
+  { text: "Read a book", completed: false, id: 2300 },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>To-Do List</h1>
+      <TodoList />
+    </main>
   );
 }
 
-export default App;
+function TodoList() {
+  const [todoItems, setTodoItems] = useState(initial_todos);
+  // const [deleteItem, setDeleteItem] = useState()
+
+  function handleAddtodoItem(todoItem) {
+    setTodoItems((todoItems) => [...todoItems, todoItem]);
+  }
+
+  function handleDeleteItem(id) {
+    setTodoItems(todoItems.filter((todoItem) => todoItem.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setTodoItems((todoItems) =>
+      todoItems.map((todoItem) =>
+        todoItem.id === id
+          ? { ...todoItem, completed: !todoItem.completed }
+          : todoItem
+      )
+    );
+  }
+
+  return (
+    <section>
+      <AddTodoForm onAddtodo={handleAddtodoItem} />
+      <ul>
+        {todoItems.map((todoItem) => (
+          <TodoItem
+            key={todoItem.id}
+            todo={todoItem}
+            onDeleteItem={handleDeleteItem}
+            onToggleItem={handleToggleItem}
+          />
+        ))}
+      </ul>
+
+      <Stats />
+    </section>
+  );
+}
+
+function TodoItem({ todo, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input type="checkbox" onChange={() => onToggleItem(todo.id)} />
+      <span
+        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      >
+        {todo.text}
+      </span>
+      <button onClick={() => onDeleteItem(todo.id)}>❌</button>
+    </li>
+  );
+}
+
+function AddTodoForm({ onAddtodo }) {
+  const [newTask, setNewTask] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!newTask) return;
+
+    const id = crypto.randomUUID();
+    const new_todo = {
+      text: newTask,
+      completed: false,
+      id,
+    };
+
+    onAddtodo(new_todo);
+    console.log(new_todo);
+
+    setNewTask("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newTask}
+        placeholder="Add a new task"
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>
+        💼 You have X tasks on your list, and you already completed X (X%)
+      </em>
+    </footer>
+  );
+}
